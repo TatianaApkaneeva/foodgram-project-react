a091004db73376b22e0cfb88e588fa61dc77b148
Татьяна Апканеева <apkaneeva@yandex.ru>
2023-03-25 18:04:19 +0300

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(
 SECRET_KEY = ('SECRET_KEY', 'my_secret_code_35h4jilz@4zqj=rq&agdol^##zgl9(vs')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
