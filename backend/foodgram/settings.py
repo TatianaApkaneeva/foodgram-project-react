cac84bf7130914fe7bc94e94baa8426200cd8844
Татьяна Апканеева <apkaneeva@yandex.ru>
2023-03-27 13:08:56 +0300

    fix11

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = ('SECRET_KEY', 'my_secret_code_35h4jilz@4zqj=rq&agdol^##zgl9(vs')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(',')
 
 AUTH_USER_MODEL = 'users.User'
 
