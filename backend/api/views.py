aebb2b6a364770534fcee0716d519bb97a2766d5
Татьяна Апканеева <apkaneeva@yandex.ru>
2023-04-01 12:44:02 +0300

    fix font3

--- backend/api/views.py
+++ backend/api/views.py
@@ -224,31 +224,37 @@ class RecipesViewSet(viewsets.ModelViewSet):
 
         buffer = io.BytesIO()
         page = canvas.Canvas(buffer)
-        pdfmetrics.registerFont(
-            TTFont('DejaVuSerif', 'DejaVuSerif.ttf', 'UTF-8'))
-        shopping_list = (
-            request.user.shopping_list.recipe.
+        pdfmetrics.registerFont(TTFont('Vera', 'Vera.ttf', 'UTF-8'))
+        x_position, y_position = 50, 800
+        shopping_cart = (
+            request.user.shopping_cart.recipe.
             values(
                 'ingredients__name',
                 'ingredients__measurement_unit'
             ).annotate(amount=Sum('recipe__amount')).order_by())
-        page.setFont('DejaVuSerif', 24)
-        page.drawString(210, 800, 'Список покупок')
-        page.setFont('DejaVuSerif', 16)
-        height = 760
-        is_page_done = False
-        for index, recipe in enumerate(shopping_list, start=1):
-            is_page_done = False
-            page.drawString(60, height, text=(
-                f'{index}. {recipe["ingredients__name"]} - '
-                f'{recipe["amount"]} '
-                f'{recipe["ingredients__measurement_unit"]}.'))
-            height -= 30
-            if height <= 40:
-                page.showPage()
-                is_page_done = True
-        if not is_page_done:
-            page.showPage()
+        page.setFont('Vera', 14)
+        if shopping_cart:
+            indent = 20
+            page.drawString(x_position, y_position, 'Cписок покупок:')
+            for index, recipe in enumerate(shopping_cart, start=1):
+                page.drawString(
+                    x_position, y_position - indent,
+                    f'{index}. {recipe["ingredients__name"]} - '
+                    f'{recipe["amount"]} '
+                    f'{recipe["ingredients__measurement_unit"]}.')
+                y_position -= 15
+                if y_position <= 50:
+                    page.showPage()
+                    y_position = 800
+            page.save()
+            buffer.seek(0)
+            return FileResponse(
+                buffer, as_attachment=True, filename=FILENAME)
+        page.setFont('Vera', 24)
+        page.drawString(
+            x_position,
+            y_position,
+            'Cписок покупок пуст!')
         page.save()
         buffer.seek(0)
         return FileResponse(buffer, as_attachment=True, filename=FILENAME)
