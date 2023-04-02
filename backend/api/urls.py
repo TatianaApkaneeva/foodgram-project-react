7b9f8eb534739b0ccbefb5a581dac224cf95db4c
Татьяна Апканеева <apkaneeva@yandex.ru>
2023-04-02 17:31:43 +0300

    fix bag 7

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (AddAndDeleteSubscribe, AddDeleteFavoriteRecipe,
-                       AddDeleteShoppingCart, AuthToken, IngredientsViewSet,
+from api.views import (AuthToken, IngredientsViewSet,
                        RecipesViewSet, TagsViewSet, UsersViewSet, set_password)
 
 app_name = 'api'
@@ -23,18 +22,6 @@ urlpatterns = [
           'users/set_password/',
           set_password,
           name='set_password'),
-     path(
-          'users/<int:user_id>/subscribe/',
-          AddAndDeleteSubscribe.as_view(),
-          name='subscribe'),
-     path(
-          'recipes/<int:recipe_id>/favorite/',
-          AddDeleteFavoriteRecipe.as_view(),
-          name='favorite_recipe'),
-     path(
-          'recipes/<int:recipe_id>/shopping_cart/',
-          AddDeleteShoppingCart.as_view(),
-          name='shopping_cart'),
      path('', include(router.urls)),
      path('', include('djoser.urls')),
      path('auth/', include('djoser.urls.authtoken')),
