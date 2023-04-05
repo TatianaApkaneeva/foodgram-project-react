24b6b0ce567d431664ab6c78738ee7fb561a54ae
Татьяна Апканеева <apkaneeva@yandex.ru>
2023-04-05 17:10:55 +0300

    fix urls and subs4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -322,11 +322,6 @@ class SubscribeSerializer(serializers.ModelSerializer):
             'email', 'id', 'username', 'first_name', 'last_name',
             'is_subscribed', 'recipes', 'recipes_count',)
     
-    def get_is_subscribed(self, obj):
-        return Subscribe.objects.filter(
-            user=obj.user, author=obj.author
-        ).exists()
-    
     def get_recipes(self, obj):
         request = self.context.get('request')
         limit = request.GET.get('recipes_limit')
@@ -336,6 +331,3 @@ class SubscribeSerializer(serializers.ModelSerializer):
         return SubscribeRecipeSerializer(
             recipes,
             many=True).data
-    
-    def get_recipes_count(self, obj):
-        return Recipe.objects.filter(author=obj.author).count()
